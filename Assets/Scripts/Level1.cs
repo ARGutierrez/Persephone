cd82e78fde8ca54e402559102e9188a5901be480
Fierfek <geekssw@yahoo.com>
2014-11-10 22:40:18 -0800

    Undo

---
+++ Assets/Scripts/Level1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		while (ObjectPool.instance.pooledObjects == null) {
+		}
+		while (ObjectPool.instance.pooledObjects.Length < 1) {
+		}
+		ObjectPool.instance.GetObjectForType ("Player", true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
