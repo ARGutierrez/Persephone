a0ddbb1db7768c896114394fad37c2f0f337dc38
Fierfek <geekssw@yahoo.com>
2014-11-17 14:08:31 -0800

    UI Start
    1. Added menu scene with a button to start game.
    
    2. Added minion buttons on bottom. First one spawns skeleton.
    
    All UI's will respond to scaling by changing the numbers the unity
    editor.
    
    Fixed minor spawn bug where it would cause an error if there were no
    objects to despawn.

--- Assets/Scripts/Entities/Will.cs
+++ Assets/Scripts/Entities/Will.cs
@@ -4,11 +4,17 @@ using System.Collections;
 public static class Will
 {
 	static int maxWill = 0, will = 0;
+	public static bool full = true;
 
 	//Use this to set initial will and add or subtract from total ammount.
 	public static void modifyWill(int mod) {
 		maxWill += mod;
 		will += mod;
+
+		if (maxWill == will)
+			full = true;
+		else
+			full = false;
 	}
 	
 	public static bool useWill(int cost) {
@@ -18,9 +24,19 @@ public static class Will
 		} else {
 			return false;
 		}
+
+		if (maxWill == will)
+			full = true;
+		else
+			full = false;
 	}
 
 	public static void returnWill(int cost) {
 		will += cost;
+
+		if (maxWill == will)
+			full = true;
+		else
+			full = false;
 	}
 }
