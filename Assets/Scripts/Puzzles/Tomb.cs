10c8c5a97de402b44707f4ae14b0d986b86306df
Aaron Gutierrez <argutierrez92@gmail.com>
2014-11-17 17:14:11 -0800

    Added puzzle #0 and basic puzzle scripts

---
+++ Assets/Scripts/Puzzles/Tomb.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tomb : BasicPuzzleTrigger {
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void Interact()
+    {
+        int numTriggers = triggers.Length;
+        if (numTriggers > 0)
+        {
+            foreach (GameObject go in triggers)
+            {
+                go.SetActive(false);
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Interact();
+        }
+    }
+}
